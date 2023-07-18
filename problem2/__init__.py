c6e38b9a8bc54bc889cc5c07d32d800c3f8bfe43
tikkhachatriann <139779692+tikkhachatriann@users.noreply.github.com>
2023-07-18 12:47:38 +0400

    Update __init__.py

--- problem2/__init__.py
+++ problem2/__init__.py
@@ -1,36 +1,25 @@
 class AddressBook:
 
-    def __init__(self, name: str, sur_name: str, phone_number: str):
-        self.name = name
-        self.surName = sur_name
-        self.phoneNumber = phone_number
+    def __init__(self):
         self.address_book = {}
-        if self.phoneNumber.startswith("+374"):
-            print("Armenian number")
-            self.phoneNumber = phone_number
-        elif self.phoneNumber.startswith("+7"):
-            print("Russian number")
-            self.phoneNumber = phone_number
-        else:
-            print("Another area code")
-            self.phoneNumber = phone_number
+
+    def create_user(self, phone_number, name,surname):
+        self.address_book[phone_number] = {name: surname}
 
 
 def main():
-    address_book_ = {}
-    book = AddressBook
+    book = AddressBook()
     while True:
-        book.name = input('type your name: ')
-        book.sur_name = input('type your sur_name : ')
-        book.phone_number = input('type your phone_number: ')
+        name = input('type your name: ')
+        sur_name = input('type your sur_name : ')
+        phone_number = input('type your phone_number: ')
         exit = input("if you want to close addressBook please type 'exit' if not type '+'")
+        book.create_user(phone_number, name, sur_name)
         if exit == "exit":
-            address_book_[book.phone_number] = {book.name: book.sur_name}
-            print(address_book_)
+            print(book.address_book)
             break
         else:
-            address_book_[book.phone_number] = {book.name: book.sur_name}
-            print(address_book_)
+            print(book.address_book)
 
 
 if __name__ == "__main__":
