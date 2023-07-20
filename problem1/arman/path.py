e173fab12317841edb7dac1ac570ee173ccd9bfe
tikkhachatriann <139779692+tikkhachatriann@users.noreply.github.com>
2023-07-20 15:27:11 +0400

    Update path.py

--- problem1/arman/path.py
+++ problem1/arman/path.py
@@ -10,25 +10,31 @@ def get_info(path):
     for dirPath, dirNames, fileNames in os.walk(path):
         steps = dirPath.split("/")
         depths = len(steps)
+
         list_for_path.append(dirPath)
         list_for_size.append(os.path.getsize(dirPath))
         list_for_depth.append(depths)
 
     list_for_path.sort()
     list_for_size.sort()
-    
+
     while True:
-        list_input = input("if you want sort by path press 1 if by size press 2 if by depths press 3")
-        if list_input == "1":
+        press_dig = input(
+            "if you want sort by path "
+            "press 1 if by size press 2 "
+            "if you want see count of maximum depths press 3"
+        )
+
+        if press_dig == "1":
             print(f"The list of path: {list_for_path}")
-        elif list_input == "2":
+        elif press_dig == "2":
             print(f"The list size of path: {list_for_size}")
-        elif list_input == "3":
+        elif press_dig == "3":
             print(f"The maximum depths of path: {max(list_for_depth)}")
         else:
             break
-    
-    return list_for_path, list_for_size,list_for_size
+
+    return list_for_path, list_for_size, list_for_size
 
 
 if __name__ == "__main__":
