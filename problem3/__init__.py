f38c2b41a942c82f79a1b5ecec0e1058944c3a20
tikkhachatriann <139779692+tikkhachatriann@users.noreply.github.com>
2023-07-25 12:04:11 +0400

    Update __init__.py

--- problem3/__init__.py
+++ problem3/__init__.py
@@ -27,37 +27,82 @@ arm_nums = {
 
 
 # checking last character of number
-def get_last_char(last_digit):
+def get_last_char(last_digit: str) -> str:
+    """
+    Get last char
+
+    Parameters
+    ----------
+    last_digit : str
+
+    Returns
+    -------
+    str
+
+    """
     return arm_special_num[1] if last_digit == "1" else arm_nums[last_digit]
 
 
 # checking teen numbers from the end
-def check_teen_nums(digits):
+def check_teen_nums(digits_list: list) -> str:
+    """
+    Check teen nums
 
-    if digits[0] == "0":
-        return f"{get_last_char(digits[1])}"
+    Parameters
+    ----------
+    digits_list : list
 
-    separator = "ն" if digits[0] == "1" and digits[1] != "0" else ""
+    Returns
+    -------
+    str
 
-    return f"{arm_nums[digits[0] + '0']}{separator}" \
-           f"{get_last_char(digits[1])}"
+    """
+    if digits_list[0] == "0":
+        return f"{get_last_char(digits_list[1])}"
+
+    separator = "ն" if digits_list[0] == "1" and digits_list[1] != "0" else ""
+
+    return f"{arm_nums[digits_list[0] + '0']}{separator}" \
+           f"{get_last_char(digits_list[1])}"
 
 
-# checking hundred numbers from the end
-def check_hundred_nums(digits):
+# checking a hundred numbers from the end
+def check_hundred_nums(digits_list: list) -> str:
+    """
+    Check hundred nums
 
-    if digits[-3] == "0":
-        return f"{check_teen_nums(digits[-2:])}"
+    Parameters
+    ----------
+    digits_list : str
 
-    separator = "" if digits[-3] == "1" else arm_nums[digits[0]]
+    Returns
+    -------
+    str
+
+    """
+    if digits_list[-3] == "0":
+        return f"{check_teen_nums(digits_list[-2:])}"
+
+    separator = "" if digits_list[-3] == "1" else arm_nums[digits_list[0]]
 
     return f"{separator} {arm_nums['100']} " \
-           f"{check_teen_nums(digits[-2:])}"
+           f"{check_teen_nums(digits_list[-2:])}"
 
 
-# checking thousand numbers
-def check_thousand(digits_list):
+# checking a thousand numbers
+def check_thousand(digits_list: list) -> str:
+    """
+    Check thousand
 
+    Parameters
+    ----------
+    digits_list : list
+
+    Returns
+    -------
+    str
+
+    """
     digit_number = "" if (digits_list[-4] == "0" or len(digits_list) > 4) \
         else arm_nums[digits_list[0]]
 
@@ -66,19 +111,55 @@ def check_thousand(digits_list):
 
 
 # checking ten thousand numbers
-def get_ten_thousand(digits_list):
+def get_ten_thousand(digits_list: list) -> str:
+    """
+    Get ten thousand
+
+    Parameters
+    ----------
+    digits_list : list
+
+    Returns
+    -------
+    str
+
+    """
     return f"{check_teen_nums(digits_list[-5:-3])} " \
            f"{check_thousand(digits_list)}"
 
 
-# checking hundred thousand numbers
-def get_hundred_thousand(digits_list):
+# checking a hundred thousand numbers
+def get_hundred_thousand(digits_list: list) -> str:
+    """
+    Check hundred nums
+
+    Parameters
+    ----------
+    digits_list : list
+
+    Returns
+    -------
+    str
+
+    """
     return f"{check_hundred_nums(digits_list[-6:-3])} " \
            f"{check_thousand(digits_list)}"
 
 
-# checking million numbers
-def check_million(digits_list):
+# checking a million numbers
+def check_million(digits_list: list) -> str:
+    """
+    Check million
+
+    Parameters
+    ----------
+    digits_list : list
+
+    Returns
+    -------
+    str
+
+    """
     start_million = f"{get_last_char(digits_list[0])} {arm_nums['1000000']} "
 
     if check_hundred_nums(digits_list[-6:-3]) == "":
@@ -88,18 +169,55 @@ def check_million(digits_list):
 
 
 # checking ten millions numbers
-def get_ten_million(digits_list):
+def get_ten_million(digits_list: list) -> str:
+    """
+    Check ten million
+
+    Parameters
+    ----------
+    digits_list : list
+
+    Returns
+    -------
+    str
+
+    """
     return f"{check_teen_nums(digits_list[-8:-6])} {arm_nums['1000000']} " \
            f"{get_hundred_thousand(digits_list)}"
 
 
-def get_hundred_million(digits_list):
+def get_hundred_million(digits_list: list) -> str:
+    """
+    Check hundred million
+
+    Parameters
+    ----------
+    digits_list : list
+
+    Returns
+    -------
+    str
+
+    """
     return f"{check_hundred_nums(digits_list[:3])} {arm_nums['1000000']} " \
            f"{get_hundred_thousand(digits_list)}"
 
 
 # getting result vor for a given number
-def get_final_result(digits_list):
+def get_final_result(digits_list: list) -> list:
+    """
+    Get final result
+
+    Parameters
+    ----------
+    digits_list : list
+
+    Returns
+    -------
+    list
+
+    """
+
     final_list = []
 
     if len(digits_list) == 9:
@@ -145,6 +263,4 @@ if __name__ == "__main__":
             break
         else:
             print("".join(get_final_result(number)))
-
-
-
+            
