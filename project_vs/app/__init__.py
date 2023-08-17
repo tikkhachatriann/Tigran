7f3a8bf69c0421e425c78757369ad74e8019be70
tikkhachatriann <139779692+tikkhachatriann@users.noreply.github.com>
2023-08-17 16:10:19 +0400

    Add files via upload

---
+++ project_vs/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_mail import Mail
+
+
+app = Flask(__name__)
+
+app.config.update(
+            MAIL_SERVER="smtp.Gmail.com",
+            MAIL_PORT=587, 
+            MAIL_USE_TLS=True,
+            MAIL_USERNAME="tigran.khachatryan@vxsoft.com",
+            MAIL_PASSWORD="asoktlidfgunnbki"
+)
+
+mail = Mail(app)
+    
+from app.view import routes
+
+
+
+
+
